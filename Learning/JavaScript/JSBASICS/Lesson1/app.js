ee4ae5c9c36088861a897b23eef04437b7cadc14
root <root@GarrickLaptop.localdomain>
2025-08-08 14:28:34 -0400

    Beggining to learn java script

---
+++ Learning/JavaScript/JSBASICS/Lesson1/app.js
@@ -0,0 +1,57 @@
+//DOM manipulation
+
+
+//Styling Elements --------------------------------------
+// const title = document.querySelector('#main-heading');
+
+// title.style.color = 'red';
+
+// const listItems = document.querySelectorAll('.list-items');
+
+
+// for(i = 0; i < listItems.length; i++){
+//     listItems[i].style.fontSize = '3rem';
+// }
+
+// console.log(listItems);
+
+//Creating Elements-----------------------------------------------
+
+
+// const ul = document.querySelector('ul');
+// const li = document.createElement('li');
+
+
+
+
+//Adding Elements--------------------
+
+// ul.append(li);
+
+
+//Seleceting Elements--------------------------------------------------------------
+//GetElementById()
+
+//const title = document.getElementById('main-heading');
+//console.log(title);
+
+// GetElementByClassName()
+
+//const listItems = document.getElementsByClassName('list-items');
+//console.log(listItems);
+
+// GetElementsByTagName
+
+//const listItems1 = document.getElementsByTagName('li');
+//console.log(listItems1);
+
+// querySelector()
+
+//const container = document.querySelector('div');
+//console.log(container);
+
+//querySelectorALL()
+
+// const container = document.querySelectorAll('div');
+
+// console.log(container);
